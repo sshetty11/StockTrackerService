1faf01f8e37281be79cb47429cfdafd1f6b981ab
sshetty11 <shilparshetty11@gmail.com>
2017-01-30 10:04:31 -0800

    Minor changes to selenium test files

--- integration/StockTrackerUINegativeTest.java
+++ integration/StockTrackerUINegativeTest.java
@@ -7,61 +7,37 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class FirstTest {
+
+public class StockTrackerUINegativeTest {
 	@Test
-	public void FirstTestCase() throws InterruptedException{
+	public void testDefault() throws InterruptedException{
 		WebDriver driver = new FirefoxDriver();
 		String url = "http://localhost:8000/index.html";
 		String title = "Stock Tracker";
 		String stockName = "AMZN";
 		String btnValue = "Stock Value";
-		//Launch the Online Store Website
+
+		//Launch the Online Website
 		driver.get(url);
 		WebElement weStockName = driver.findElement(By.id("input-0"));
-		weStockName.sendKeys(stockName);
-		WebElement body = 		driver.findElement(By.tagName("body"));
-		body.click();
-		WebElement btnSubmit = driver.findElement(By.id("btnSubmit"));
-		btnSubmit.click();
 		WebElement divStock = driver.findElement(By.id("divStock"));
 		WebElement divNews = driver.findElement(By.id("listGroup"));
 		WebElement stockValue = driver.findElement(By.id("stockValue"));
 
-		
-		
-
 		// Verify the title
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle, title);
+
 		//Verify the url
 		String actualUrl = driver.getCurrentUrl();
 		Assert.assertEquals(actualUrl, url);
 
-//		//Verify the text value
-		Assert.assertEquals(weStockName.getAttribute("value"), stockName);
-//		//Verify the button text
-		Assert.assertEquals(btnSubmit.getAttribute("value"), btnValue);
-
-//		//Verify if the stock value is displayed
-		Assert.assertEquals(divStock.isDisplayed(), true);
-		Assert.assertEquals(divNews.isDisplayed(), true);
-
-//		
-//		//Verify if stock value is numeric
-		Assert.assertEquals(isNumeric(stockValue.getText()), true);
-
-
-
-			//Wait for 5 Sec
+		//Verify if the stock value is displayed
+		Assert.assertEquals(divStock.isDisplayed(), false);
+		Assert.assertEquals(divNews.isDisplayed(), false);
+		
+		//Wait for 5 Sec
 		Thread.sleep(5);
-
-		// Close the driver
 		driver.quit();
-		driver = null;
 	}
-	public static boolean isNumeric(String str){
-		return java.util.regex.Pattern.matches("^([+-]?\\d*\\.?\\d*)$", str);
-
-	}
-
 }
